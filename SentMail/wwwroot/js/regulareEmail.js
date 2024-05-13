5c6cda4532a6f26f7ee588c01154c885fb63916c
HaiAnh <vuduyhaianh.123@gmail.com>
2024-05-13 11:13:08 +0700

    push3

---
+++ SentMail/wwwroot/js/regulareEmail.js
@@ -0,0 +1,6 @@
+﻿var regexEmailAddress = /[!#-9A-~]+@+[a-z0-9]+.+[^.]$/;
+function validateEmail(val) {
+        var exp = new RegExp(regexEmailAddress);
+    exp.Multiline = true;
+    return exp.test(val);
+}
